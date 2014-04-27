9626e562b3ce9ce5fcd6f57dbbfdc4f066413ee2
akashkrishnan <ak@aakay.net>
2014-04-27 02:25:18 -0400

    Cut down 4 loc

--- CERN-Application-2014/Program.cs
+++ CERN-Application-2014/Program.cs
@@ -30,11 +30,7 @@ namespace CERN_Application_2014
         private static int SumOfDigits(BigInteger n)
         {
             int sum = 0;
-            while (n > 0)
-            {
-                sum += (int)(n % 10);
-                n /= 10;
-            }
+            for (; n > 0; sum += (int)(n % 10), n /= 10) ;
             return sum;
         }
     }
